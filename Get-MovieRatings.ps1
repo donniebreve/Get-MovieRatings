8d1817b19d373f0d6eecebda1adb8cacc0e29b45
donniebreve <donniebreve@gmail.com>
2018-03-14 23:19:25 -0700

    Update Get-MovieRatings.ps1

--- Get-MovieRatings.ps1
+++ Get-MovieRatings.ps1
@@ -18,7 +18,7 @@ function Get-MovieRatings {
 
     Write-Host "Getting primary information"
     $movie = Get-IMDbInformation $title
-    $movie.RT = Get-RTInformation $movie.Title $movie.Year
+    $movie.RT = Get-RTInformation $title $movie.Year
     
     Write-Host "Result:"
     $movie
@@ -35,13 +35,14 @@ function Get-IMDbInformation {
     )
 
     # encode the title
-    $encodedTitle = $title -replace "-", " "
+    $encodedTitle = $title
     $encodedTitle = $encodedTitle -replace " ", "+"
     $encodedTitle = $encodedTitle -replace "&amp;", "%26"
 
     # search IMDb
     $url = "http://www.imdb.com/find?q=$encodedTitle&s=all"
-    $response = Invoke-WebRequest $url
+    write-host "imdb: $url" -ForegroundColor "DarkGray"
+    $response = Invoke-WebRequest $url -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
     
     # check for no results
     $elements = $response.ParsedHtml.getElementsByClassName('noresults')
@@ -61,7 +62,8 @@ function Get-IMDbInformation {
             $result.Title = $match.Groups[2].value
             $result.Year = $match.Groups[3].value
             [void]$results.Add($result)
-            Write-Host "$($i + 1): $($result.Title), $($result.Year)"
+            Write-Host -NoNewline "$($i + 1): $($result.Title), $($result.Year)"
+            Write-Host " http://www.imdb.com$($result.Url)" -ForegroundColor "DarkGray"
         }
     }
     if ($results.Count -gt 1) {
@@ -114,17 +116,17 @@ function Get-RTInformation {
         [Parameter(Mandatory=$true)]
         [String]$title,
         [Parameter(Mandatory=$true)]
-        [String]$year
+        [Int32]$year
     )
 
     # encode the title
-    $encodedTitle = $title -replace "-", " "
+    $encodedTitle = $title
     $encodedTitle = $encodedTitle -replace " ", "+"
     $encodedTitle = $encodedTitle -replace "&amp;", "%26"
 
     # search RT
     $url = "https://www.rottentomatoes.com/search/?search=$encodedTitle"
-    Write-Host "rt: searching $url" -ForegroundColor "DarkGray"
+    Write-Host "rt: $url" -ForegroundColor "DarkGray"
     $response = Invoke-WebRequest $url
 
     # check for results
@@ -137,10 +139,12 @@ function Get-RTInformation {
         $url = $null
         $json = ConvertFrom-Json $match.Groups[1]
         for ($i = 0; $i -lt $json.Length; $i++) {
-            $item = $json[$i]
-            $y = [convert]::ToInt32($item.year)
-            Write-Host "rt: checking against $($item.name) $($item.year)" -ForegroundColor "DarkGray"
-            if (($item.name.Contains($title) -or $title.Contains($item.name)) -and ($y -eq $year -or $y + 1 -eq $year)) {
+            $it = $json[$i].name
+            $iy = [convert]::ToInt32($json[$i].year)
+            $distance = LDCompare $title $it $true
+            Write-Host "rt: comparing $it $iy https://www.rottentomatoes.com$($json[$i].url)" -ForegroundColor "DarkGray"
+            write-host "rt: distance $distance, percent $([Double]$distance/$title.Length)" -ForegroundColor "DarkGray"
+            if (($distance/$title.Length -lt 0.2) -and ($iy -eq $year -or $iy -eq ($year + 1))) {
                 Write-Host "rt: matched $($item.name) $($item.year)" -ForegroundColor "DarkGray"
                 $url = "https://www.rottentomatoes.com$($json[$i].url)"
                 break
@@ -177,4 +181,53 @@ function Get-RTInformation {
         else { $result += "/?" }
         return $result
     }
+}
+
+# https://www.codeproject.com/Tips/102192/Levenshtein-Distance-in-Windows-PowerShell
+function LDCompare {
+
+    param(
+        [String] $first,
+        [String] $second,
+        [Switch] $ignoreCase
+    )
+
+    $len1 = $first.length
+    $len2 = $second.length
+
+    # if either string has length of zero, the # of edits/distance between them is simply the length of the other string
+    if ($len1 -eq 0) { return $len2 }
+    if ($len2 -eq 0) { return $len1 }
+
+    # make everything lowercase if ignoreCase flag is set
+    if($ignoreCase -eq $true) {
+        $first = $first.ToLower()
+        $second = $second.ToLower()
+    }
+
+    # create 2d Array to store the "distances"
+    $dist = new-object -type 'int[,]' -arg ($len1+1),($len2+1)
+
+    # initialize the first row and first column which represent the 2 strings we're comparing
+    for ($i = 0; $i -le $len1; $i++) { $dist[$i,0] = $i }
+    for ($j = 0; $j -le $len2; $j++) { $dist[0,$j] = $j }
+
+    # compare
+    $cost = 0
+    for($i = 1; $i -le $len1;$i++) {
+        for($j = 1; $j -le $len2;$j++) {
+            if ($second[$j-1] -ceq $first[$i-1]) { $cost = 0 }
+            else { $cost = 1 }
+    
+            # The value going into the cell is the min of 3 possibilities:
+            # 1. The cell immediately above plus 1
+            # 2. The cell immediately to the left plus 1
+            # 3. The cell diagonally above and to the left plus the 'cost'
+            $tempmin = [System.Math]::Min(([int]$dist[($i - 1), $j] + 1), ([int]$dist[$i, ($j - 1)] + 1))
+            $dist[$i, $j] = [System.Math]::Min($tempmin, ([int]$dist[($i - 1), ($j - 1)] + $cost))
+        }
+    }
+
+    # the distance is stored in the bottom right cell
+    return $dist[$len1, $len2];
 }
